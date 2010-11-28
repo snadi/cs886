a4dac2d104735ac126754c00033a639b415be9d7
karimhamdan@gmail.com <karimhamdan@gmail.com@b76c1b66-5e7c-c3f3-06f8-08a990a65ca8>
2010-11-28 16:27:29 +0000

    todo

--- cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/DataPreprocessor.java
+++ cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/DataPreprocessor.java
@@ -1,5 +1,23 @@
 package ca.uwaterloo.bhp.cfg;
 
+import soot.Scene;
+import soot.SootClass;
+import soot.toolkits.graph.BriefBlockGraph;
+
 public class DataPreprocessor {
+	
+	public static BriefBlockGraph constructCFG() {
+	    Scene.v().loadClassAndSupport("java.lang.String");
+	    Scene.v().loadNecessaryClasses();
+		SootClass sc = Scene.v().getSootClass("java.lang.String");
+		sc.setApplicationClass();
+		return new BriefBlockGraph(sc.getMethodByName("charAt").retrieveActiveBody());
+	}
+	
+	public static void main(String args[]) {
+		BriefBlockGraph cfg = constructCFG();
+		
+		System.out.println(cfg.getHeads());
+	}
 
 }
