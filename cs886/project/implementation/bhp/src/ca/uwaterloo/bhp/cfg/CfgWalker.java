a5d3e37e264f5b730e4041c9e3562db0d1a5fb11
sarah.ezznadi <sarah.ezznadi@b76c1b66-5e7c-c3f3-06f8-08a990a65ca8>
2010-11-30 15:25:21 +0000

    more files

---
+++ cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/CfgWalker.java
@@ -0,0 +1,11 @@
+package ca.uwaterloo.bhp.cfg;
+
+import soot.toolkits.graph.BriefBlockGraph;
+
+public class CfgWalker {
+	
+	public static void process(BriefBlockGraph cfg) {
+		
+	}
+
+}
---
+++ cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/FeatureNames.java
@@ -0,0 +1,19 @@
+package ca.uwaterloo.bhp.cfg;
+
+public enum FeatureNames {
+	EQUAL("=="),
+	NEW("new"),
+	THIS("this"),
+	VARIABLES("vars"),
+	ASSIGNMENTS("="),
+	DEREFERNCES("."),
+	FIELDS("fields"),
+	FIELDS_WRITTEN("fields written"),
+	INVOCATIONS("invocations"),
+	GOTO("goto"),
+	IF("if"),
+	LOCAL_INVOCATIONS("local invocations"),
+	LOCAL_VARIABLES("local variables"),
+	
+
+}
