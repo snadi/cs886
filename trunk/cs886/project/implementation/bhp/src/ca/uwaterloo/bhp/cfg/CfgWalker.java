854a7df334717c352d6a4014fbef0b04649a6c93
karimhamdan@gmail.com <karimhamdan@gmail.com@b76c1b66-5e7c-c3f3-06f8-08a990a65ca8>
2010-12-05 21:50:20 +0000

    given some input files (zip, jar, class, java), execution paths are generated, each with its own set of features

--- trunk/cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/CfgWalker.java
+++ trunk/cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/CfgWalker.java
@@ -1,7 +1,11 @@
 package ca.uwaterloo.bhp.cfg;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Stack;
 
 import soot.toolkits.graph.Block;
 import soot.toolkits.graph.BriefBlockGraph;
@@ -12,15 +16,55 @@ public class CfgWalker {
 		Collection<ExecutionPath> result = new ArrayList<ExecutionPath>();
 		
 		for(Block head : cfg.getHeads()) {
-			boolean done = false;
+			Stack<Block> path = new Stack<Block>();
+			HashSet<Block> seen = new HashSet<Block>();
 			
-			while(!done) {
-				head.
-			}
+			Collection<ExecutionPath> allPaths = new ArrayList<ExecutionPath>();
+			search(head, head, path, seen, allPaths);
+			
+			result.addAll(allPaths);
 		}
 		
 		return result;
-		
 	}
-
+	
+	private static void search(Block head, Block block, Stack<Block> path, HashSet<Block> seen, Collection<ExecutionPath> allPaths) {
+		if(block.getSuccs().size() == 0) {
+			ExecutionPath p = new ExecutionPath();
+			p.blocks().add(head);
+			if(path.size() > 0) {
+				p.blocks().addAll(Arrays.asList(path.toArray(new Block[1])));
+			}
+			allPaths.add(p);
+		}
+	    
+		seen.clear();
+		seen.addAll(Collections.list(path.elements()));
+		
+		if(stuck(block, path, seen)) {
+			return;
+		}
+		
+		for(Block b : block.getSuccs()) {
+			path.push(b);
+			search(head, b, path, seen, allPaths);
+			path.pop();
+		}
+	}
+	
+	private static boolean stuck(Block block, Stack<Block> path, HashSet<Block> seen) {
+		if(block.getSuccs().size() == 0) {
+			return false;
+		}
+		
+		for(Block b : block.getSuccs()) {
+			if(!seen.contains(b)) {
+				seen.add(b);
+				if(!stuck(b, path, seen)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
--- trunk/cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/ExecutionPath.java
+++ trunk/cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/cfg/ExecutionPath.java
@@ -1,10 +1,15 @@
 package ca.uwaterloo.bhp.cfg;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+
+import soot.toolkits.graph.Block;
 
 public class ExecutionPath {
 	
 	private HashMap<FeatureName, Feature> features;
+	private Collection<Block> blocks;
 	
 	public ExecutionPath() {
 		features = new HashMap<FeatureName, Feature>();
@@ -13,11 +18,36 @@ public class ExecutionPath {
 		for(FeatureName key : FeatureName.values()) {
 			features.put(key, new Feature(key));
 		}
+		
+		blocks = new ArrayList<Block>();
 	}
 
 	public HashMap<FeatureName, Feature> features() {
 		return features;
 	}
 	
+	public Collection<Block> blocks() {
+		return blocks;
+	}
+	
+	public String pathToString() {
+		StringBuffer str = new StringBuffer();
+		for(Block b : blocks) {
+			str.append(b.getIndexInMethod());
+			str.append(", ");
+		}
+		return str.replace(str.lastIndexOf(","), str.lastIndexOf(" "), "").toString();
+	}
+	
+	public String featuresToString() {
+		StringBuffer str = new StringBuffer();
+		for(FeatureName f : features.keySet()) {
+			str.append(features.get(f).getName());
+			str.append("=");
+			str.append(features.get(f).getCount());
+			str.append(", ");
+		}
+		return str.replace(str.lastIndexOf(","), str.lastIndexOf(" "), "").toString();
+	}
 	
 }
