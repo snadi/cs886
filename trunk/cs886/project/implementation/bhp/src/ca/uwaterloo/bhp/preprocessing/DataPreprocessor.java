aa3e0df1cec7c58be00d09b51f433587836e9dc8
karimhamdan@gmail.com <karimhamdan@gmail.com@b76c1b66-5e7c-c3f3-06f8-08a990a65ca8>
2010-12-07 04:19:06 +0000

    arff 100 100

--- trunk/cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/preprocessing/DataPreprocessor.java
+++ trunk/cs886/project/implementation/bhp/src/ca/uwaterloo/bhp/preprocessing/DataPreprocessor.java
@@ -19,88 +19,96 @@ import ca.uwaterloo.bhp.weka.ArffWriter;
 
 public class DataPreprocessor {
 	
-	private static NoSearchingClassProvider _provider;
-	private static Scene _scene;
-	
 	public static final String INPUT_DIRECTORY = System.getProperty("user.dir") + File.separator + "cs886" + File.separator + "input";
+	public static final String LIBRARY_DIRECTORY = System.getProperty("user.dir") + File.separator + "cs886" + File.separator + "input" + File.separator + "lib";
 	public static final String ARFF_DIRECTORY = System.getProperty("user.dir") + File.separator + "cs886" + File.separator + "arff";
 	
 	public static void run() throws IOException {
-		// Initialize some parameters for Soot
-		init();
-		
-		// Load the classes
-		Collection<SootClass> inputClasses = loadClasses();
-		
-		// Generate the features
-		generateFeatures(inputClasses);		
-	}
-	
-	private static void generateFeatures(Collection<SootClass> inputClasses) throws IOException {
-		// For each soot class, generate control flow graphs for its methods, and process them
-		for(SootClass sc : inputClasses){
-			for(BriefBlockGraph cfg : CfgGenerator.generate(sc)){
-				Collection<ExecutionPath> paths = CfgWalker.process(cfg);
-				for(ExecutionPath path : paths) {
-					FeatureExtractor.extractFeatures(path);
-				}
-				
-				ArffWriter writer = new ArffWriter(ARFF_DIRECTORY, sc.getName().replaceAll("\\.", "_"), cfg.getBody().getMethod().getNumber(), paths);
-				writer.write();
-			}
-		}
-	}
-	
-	private static SootClass loadClassByCanonicalName(String className) {
-		_scene.loadClassAndSupport(className);
-	    _scene.loadNecessaryClasses();
-		SootClass sc = _scene.loadClass(className, SootClass.BODIES);
-		sc.setApplicationClass();
-		
-		return sc;
-	}
-	
-	private static Collection<SootClass> loadSootClasses() {
-		// Load SootClasses for the input files
-		Collection<SootClass> classes = new ArrayList<SootClass>();
-	    for(String className : _provider.getClassNames()) {
-	    	//_scene.loadClassAndSupport(className);
-	    	SootClass sc = _scene.loadClass(className, SootClass.BODIES);
-	    	_scene.loadNecessaryClasses();
-	    	sc.setApplicationClass();
-	    	classes.add(sc);
-	    }
-	    
-	    // Set the class provider for Soot
-		soot.SourceLocator.v().setClassProviders(Collections.singletonList((ClassProvider) _provider));
-		
-	    //addCommonDynamicClasses(_scene, _provider);
-		_scene.loadNecessaryClasses();
-		
-		return classes;
-	}
-	
-	private static Collection<SootClass> loadClasses() throws IOException {
-		// Fetch the input files
-		for(File file : new File(INPUT_DIRECTORY).listFiles()) {
-    		if(file.getName().endsWith(".jar") || file.getName().endsWith(".zip")) {
-    	        System.out.println("Adding archive: " + file.getName());
-    	        _provider.addArchive(file);
-    		} else if (file.getName().endsWith(".class")) {
-    			System.out.println("Adding file: " + file.getName());
-    	        _provider.addClass(file);
-    		}
-    	}
-		
-		return loadSootClasses();
-	}
-	
-	private static void init() {
 		// Reset Soot
 		G.reset();
 		
 		// Create the Soot-related members
-		_provider = new NoSearchingClassProvider();
-		_scene = Scene.v();
+		NoSearchingClassProvider provider = new NoSearchingClassProvider();
+		
+		// Fetch the input files
+		for(File file : new File(INPUT_DIRECTORY).listFiles()) {
+    		if(file.getName().endsWith(".jar") || file.getName().endsWith(".zip")) {
+    	        System.out.println("Adding archive: " + file.getName());
+    	        provider.addArchive(file);
+    	        //provider.addArchiveForResolving(file);
+    		} else if (file.getName().endsWith(".class")) {
+    			System.out.println("Adding file: " + file.getName());
+    	        provider.addClass(file);
+    		}
+    	}
+		
+		// Fetch all library jars
+		for(File file : new File(LIBRARY_DIRECTORY).listFiles()) {
+			provider.addArchiveForResolving(file);
+    	}
+		
+		// Set the class provider for Soot
+		soot.SourceLocator.v().setClassProviders(Collections.singletonList((ClassProvider) provider));
+		Scene scene = Scene.v();
+
+		Collection<SootClass> classes = new ArrayList<SootClass>();
+	    for(String className : provider.getClassNames()) {
+	    	scene.loadClass(className, SootClass.SIGNATURES);
+	    	//scene.loadClassAndSupport(className);
+	    	//scene.loadNecessaryClasses();
+	    	SootClass c = scene.loadClass(className, SootClass.BODIES);
+	    	c.setApplicationClass();
+	    	classes.add(c);
+	    }
+	    
+	    addCommonDynamicClasses(scene, provider);
+	    scene.loadNecessaryClasses();
+
+		// Generate the features
+		generateFeatures(classes, 20);
+		
+		
+	}
+	
+	private static void generateFeatures(Collection<SootClass> inputClasses, int cfgNodeThreshold) throws IOException {
+		// For each soot class, generate control flow graphs for its methods, and process them
+		Collection<ExecutionPath> paths = new ArrayList<ExecutionPath>();
+		for(SootClass sc : inputClasses){
+			for(BriefBlockGraph cfg : CfgGenerator.generate(sc)){
+				if(cfg.size() > cfgNodeThreshold) continue;
+				for(ExecutionPath path : CfgWalker.process(cfg)) {
+					FeatureExtractor.extractFeatures(path);
+					paths.add(path);
+				}
+			}
+		}
+		
+		if(paths.size() > 0) {
+			ArffWriter writer = new ArffWriter(ARFF_DIRECTORY, "observations", paths);
+			writer.write();
+		}
+	}
+	
+	private static void addCommonDynamicClasses(Scene scene, ClassProvider provider) {
+		/* For simulating the FileSystem class, we need the implementation
+	       of the FileSystem, but the classes are not loaded automatically
+	       due to the indirection via native code.
+	     */
+	    addCommonDynamicClass(scene, provider, "java.io.UnixFileSystem");
+	    addCommonDynamicClass(scene, provider, "java.io.WinNTFileSystem");
+	    addCommonDynamicClass(scene, provider, "java.io.Win32FileSystem");
+	    
+	    /* java.net.URL loads handlers dynamically */
+	    addCommonDynamicClass(scene, provider, "sun.net.www.protocol.file.Handler");
+	    addCommonDynamicClass(scene, provider, "sun.net.www.protocol.ftp.Handler");
+	    addCommonDynamicClass(scene, provider, "sun.net.www.protocol.http.Handler");
+	    addCommonDynamicClass(scene, provider, "sun.net.www.protocol.https.Handler");
+	    addCommonDynamicClass(scene, provider, "sun.net.www.protocol.jar.Handler");
+	}
+	
+	private static void addCommonDynamicClass(Scene scene, ClassProvider provider, String className) {
+		if (provider.find(className) != null) {
+			scene.addBasicClass(className);
+		}
 	}
 }
